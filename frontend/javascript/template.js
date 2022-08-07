37537f6bf207e921c73116caf95b6f8c93e50db4
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2022-08-07 17:15:24 +0300

    İnstanceof typeof

--- frontend/javascript/template.js
+++ frontend/javascript/template.js
@@ -22,6 +22,7 @@ document.writeln("<br/> Benden sonra gelen alt satırda başlar");
 // $ ve _ baslayabilriz. Bu simgeler dışında başka bir şey kullanma sayi bile.
 // Camelcase kullanalım
 
+/*
 var degiskenAdi = 44;
 console.log(degiskenAdi);
 
@@ -32,17 +33,17 @@ console.log(_degiskenAdi55);
 var $degiskenAdi55 = "Merhabalar";
 console.log($degiskenAdi55);
 
-
-
+*/
+/*
 var sayi=44;
 var object= {};
 var dizi = [];
 var karar = true;
-
+*/
 //whoisting : Değişken türünü sonra yaz.
-degisken =  55;
-var degisken;
-console.log(degisken);
+//degisken =  55;
+//var degisken;
+//console.log(degisken);
 
 //var ==> global 
 // let ==> local ==> ES6
@@ -51,18 +52,44 @@ console.log(degisken);
 
 
 
-console.log(typeof sayi)
+//console.log(typeof sayi)
 
 
-//variable initialization
+// variable initialization
 // İlk deger ataması
-var initData = "";
-//nullpointer exception veya undefined almayız.
+// var initData = "";
+// nullpointer exception veya undefined almayız.
 
 //Atama Operatörü
 // = Atama operatörü
 // == Eşit türüne bakmadan
-// === eşit türüne bakaral
+// === eşit türüne bakarak
+
+// Kullanıcadan veri alma
+// prmpt: verisinde bize String yani kelime döner biz bunu sayıya çevirmemz gerekir.
+var userData = Number(prompt("Lütfen bir sayı giriniz"))
+console.log(userData*userData);
+
+
+
+//instanceof o verinin o türe ait olmadığını söyler.
+var variable = "Mustafa Kemal Atatürk"
+console.log(variable);
+console.log(typeof(variable));
+
+
+
+var dizi = [];
+console.log(dizi);
+console.log(typeof(dizi));
+console.log(dizi instanceof Array);
+console.log(dizi instanceof Object);
+
+
+//
+console.log(dizi instanceof Number);
+
+
 
 
 
