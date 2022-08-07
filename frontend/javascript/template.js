572860ff22bd32c8b4886562a37135c719157580
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2022-08-07 16:24:09 +0300

    atama operatorü

---
+++ frontend/javascript/template.js
@@ -0,0 +1,80 @@
+//alert("sads")
+
+//single comment : comment out
+// multiple comment
+
+/*
+console.log("Merhabalar");
+console.info("info");
+
+console.warn("warning");
+console.error("error");
+
+alert("pop-op");
+window.alert("pop-up-2");
+
+document.write("write verisi");
+document.writeln("<br/> Benden sonra gelen alt satırda başlar");
+
+*/
+
+//variable: dinamik bir değerdir.
+// $ ve _ baslayabilriz. Bu simgeler dışında başka bir şey kullanma sayi bile.
+// Camelcase kullanalım
+
+var degiskenAdi = 44;
+console.log(degiskenAdi);
+
+
+var _degiskenAdi55 = "Merhabalar";
+console.log(_degiskenAdi55);
+
+var $degiskenAdi55 = "Merhabalar";
+console.log($degiskenAdi55);
+
+
+
+var sayi=44;
+var object= {};
+var dizi = [];
+var karar = true;
+
+//whoisting : Değişken türünü sonra yaz.
+degisken =  55;
+var degisken;
+console.log(degisken);
+
+//var ==> global 
+// let ==> local ==> ES6
+
+
+
+
+
+console.log(typeof sayi)
+
+
+//variable initialization
+// İlk deger ataması
+var initData = "";
+//nullpointer exception veya undefined almayız.
+
+//Atama Operatörü
+// = Atama operatörü
+// == Eşit türüne bakmadan
+// === eşit türüne bakaral
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
