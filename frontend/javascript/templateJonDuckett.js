94a67401fabc29b84f39330459b9b99886a6671f
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2022-08-16 20:27:58 +0300

    CONSTRUCTOR NOTATION CREATING OBJECT,  IIFE

--- frontend/javascript/templateJonDuckett.js
+++ frontend/javascript/templateJonDuckett.js
@@ -26,13 +26,72 @@
 // el.textContent = colors2[0];
 
 
-let tiles = document.getElementById('tiles');
-let subTotal = tiles*7;
+// let tiles = document.getElementById('tiles');
+// let subTotal = tiles*7;
 
-let elSubTotal = document.getElementById('subTotalId');
-elSubTotal.textContent = subTotal;
+// let elSubTotal = document.getElementById('subTotalId');
+// elSubTotal.textContent = subTotal;
+
+
+// let msg = "Sign up to receive our newsletter for 10% off! "
+
+//A BASIC FUNCTION
+//  function updateMessage(msg) {
+//      let el = document.getElementById('message');
+//      el.textContent = msg; 
+
+// }
+// updateMessage(msg);
+
+
+// IIFE
+// let area = (function() {
+//     let width = 3;
+//     let height = 2;
+//     return area * width
+// }() );
+
+// LITERAL NOTATION CREATING OBJECT
+// g
+
+
+// let hotelName = hotel.name;
+// document.writeln(hotelName);
+// let roomsFree = hotel.checkAvailability();
+
+// CONSTRUCTOR NOTATION CREATING OBJECT
+
+// var hotel2 = new Object();
+// hotel2.name ="quay";
+// hotel2.rooms= 40;
+// hotel2.booked = 25;
+// hotel.checkAvailability = function () {
+//     return this.rooms - this.booked;
+//     }
 
 
 
+
+    // function Hotel(name,room,booked){
+    //     this.name = name;
+    //     this.room = room;
+    //     this.booked = booked;
+    //     this.checkAvailability = function(){
+            
+    //     }
+    //  }
+
+    // window.alert("pop-up-2")
+
+
+    //USING THE BROWSER OBJECT MODEL
+    // let msg = '<h2>Browser Window</h2> <p>width'+window.innerWidth +'</p>';
+    // msg += "<p> height:</p>" + window.innerHeight + "</p>";
+    // msg += "<h2>History</h2>"+"<p>items:</p>" +window.history.length + "</p>";
+
+    // let element = document.getElementById("info");
+    // element.innerHTML = msg;
+
+    
 
 
