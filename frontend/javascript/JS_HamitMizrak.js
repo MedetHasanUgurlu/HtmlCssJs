167a733194983d2df7371b2d997774267af94904
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2022-08-17 08:32:12 +0300

    prompt, console, instanceof, back slash

---
+++ frontend/javascript/JS_HamitMizrak.js
@@ -0,0 +1,45 @@
+// alert("Deneme");
+// window.alert("Deneme");
+
+// console.log("Hello There")
+// console.info("info");
+// console.warn("warning");
+// console.error("error");
+
+// var  ==> global
+// let  ==> local ==> ES6 (2015)
+// const==> local ==> ES6 (2015)
+
+// typeof yapısı
+// let array = [];
+// console.log(typeof array);
+
+// nullpointer exception veya undefined hatası almayız.
+// let initData = "";
+
+// Kullanıcadan veri almak.
+// Prompt String döndürür.
+// let userData = prompt("Lütfen bir sayı giriniz.");
+// console.log(userData);
+// console.log(typeof userData);
+
+// let userData2 = Number(prompt("Lütfen bir sayı giriniz."));
+// console.log(userData);
+// console.log(typeof userData);
+
+// instanceof olup olamdığını kontrol eder
+// let a = 'Lorem';
+// console.log(a);
+// console.log(a instanceof String);
+
+// BACK SLASH
+console.log("1.satır\n 2.satır");
+console.log("1.satır\n \t 2.satır");
+
+
+
+
+
+
+
+
